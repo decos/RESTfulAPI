46800a136b6d6ebfa0dd366ba3bd61c1b426a9c0
Diego Abanto <diego.abanto@ec.pe>
2017-07-18 17:45:23 -0500

    Probando la base de datos usando Laravel parte 1

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,9 +12,12 @@ class DatabaseSeeder extends Seeder {
 	 */
 	public function run()
 	{
-		Model::unguard();
+                                //Deshacer las asignaciones masivas
+		//Model::unguard();
 
 		// $this->call('UserTableSeeder');
+                                $this->call('FabricanteSeeder');
+                                $this->call('VehiculoSeeder');
 	}
 
 }
