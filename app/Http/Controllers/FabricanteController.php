eff81c607f7e454750c81015c66a9fb8968a0591
Diego Abanto <diego.abanto@ec.pe>
2017-07-19 17:22:03 -0500

    Creando Controladores para nuestra API

--- app/Http/Controllers/FabricanteController.php
+++ app/Http/Controllers/FabricanteController.php
@@ -1,5 +1,42 @@
 <?php namespace App\Http\Controllers;
 
+use App\Http\Requests\Request;
+use App\Http\Controllers\Controller;
+
 class FabricanteController extends Controller{
+        
+        public function index(){
+                return "Mostrando todos los fabricantes";
+        }
+        
+        public function create(){
+                return "Mostrando formulario para crear un fabricante";
+        }
+        
+        public function store(){
+            
+        }
+        
+        public function show($id){
+                return "mostrando fabricante con id: ".$id;
+        }
+        
+        public function edit($id){
+                return "mostrando formulario para editar el fabricante con id: ".$id;
+        }
+    
+        public function update($id){
+            
+        }
+        
+        //PATCH
+        /*public function create(){
+            
+        }*/
+        
+        public function destroy($id){
+            
+        }
+        
         
 }
